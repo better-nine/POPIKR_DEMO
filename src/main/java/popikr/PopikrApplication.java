009e1c02c9462888b530f41344e129d92cfc0bf0
better-nine <76273087+better-nine@users.noreply.github.com>
2024-03-12 10:20:33 +0900

    init
    init

---
+++ src/main/java/popikr/PopikrApplication.java
@@ -0,0 +1,13 @@
+package popikr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopikrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopikrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/popikr/PopikrApplicationTests.java
@@ -0,0 +1,13 @@
+package popikr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopikrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
